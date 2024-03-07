a69a7ea5fc8dc8423b2e185f027e48c984439704
Takemura <sunthy13361@gmail.com>
2024-03-07 20:16:38 +0100

    first cm

---
+++ ada/main.adb
@@ -0,0 +1,56 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+
+   Id_Of_Thread : Integer := 0;
+   Number_Of_Threads : Positive := 2;
+   Step : Long_Long_Integer := 2;
+
+   Flag : Boolean := False;
+   Can_Stop : Boolean := False;
+
+   pragma Atomic(Can_Stop);
+   pragma Volatile(Id_Of_Thread);
+
+
+   task type Break_Thread;
+   task type Main_Thread;
+
+   type Threads_Array is array (Positive range <>) of Main_Thread;
+
+   task body Break_Thread is
+   begin
+      delay 1.0;
+      Can_Stop := True;
+   end Break_Thread;
+
+   task body Main_Thread is
+      Sum : Long_Long_Integer := 0;
+      Steps : Integer := 0;
+   begin
+      loop
+         Steps := Steps + 1;
+         Sum := Sum + Step;
+         exit when Can_Stop;
+      end loop;
+      delay 1.0;
+      Id_Of_Thread := Id_Of_Thread + 1;
+      Put_Line("Thread: " & Id_Of_Thread'Img & " Sum: " & Sum'Img & " Steps: " & 
+Steps'Img);
+
+   end Main_Thread;
+
+   procedure Lab1(N: Positive) is
+      Threads : Threads_Array(1 .. N);
+      Break : Break_Thread;
+   begin
+      null;
+      end Lab1;
+begin
+   Put("EnteR step value: ");
+   Step := Long_Long_Integer'Value(Get_Line);
+   Put("Enter number of threads: ");
+   Number_Of_Threads := Positive'Value(Get_Line);
+   Lab1(Number_Of_Threads);
+   null;
+end Main;
---
+++ c#/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelThreadDemo
+{
+    class Program
+    {
+        private bool canStop = false;
+        private int step;
+
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.StartAsync();
+        }
+
+        async Task StartAsync()
+        {
+            Console.Write("Enter the number of threads to create: ");
+            int threadCount = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the step value: ");
+            step = int.Parse(Console.ReadLine());
+
+            List<Task<(long, int, int)>> tasks = new List<Task<(long, int, int)>>();
+            for (int i = 0; i < threadCount; i++)
+            {
+                int threadNum = i + 1;
+                tasks.Add(Task.Run(() => Calculator(threadNum)));
+            }
+
+            Task stopTask = Task.Run(() => Stoper());
+
+            await Task.WhenAll(tasks.ToArray());
+            canStop = true;
+
+            await stopTask;
+
+            foreach (var result in await Task.WhenAll(tasks))
+            {
+                Console.WriteLine($"Thread {result.Item2}: Sum: {result.Item1}, Steps: {result.Item3}");
+            }
+        }
+
+        async Task<(long, int, int)> Calculator(int threadNum)
+        {
+            long sum = 0;
+            int steps = 0;
+            do
+            {
+                sum += step;
+                steps++;
+                Thread.Sleep(10);
+            } while (!CanStop);
+
+            return (sum, threadNum, steps);
+        }
+
+        public bool CanStop { get => canStop; }
+
+        async Task Stoper()
+        {
+            await Task.Delay(30000);
+            canStop = true;
+        }
+    }
+}
